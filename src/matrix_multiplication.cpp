23f3390d22b4bd927faacb6be338592134bc158a
Mateusz Warzyński <mathew.warzynski@gmail.com>
2019-05-24 00:31:41 +0200

    Added communication example - src

--- src/matrix_multiplication.cpp
+++ src/matrix_multiplication.cpp
@@ -5,12 +5,17 @@ int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
 
     int num_processes, rank;
-
     MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    std::cout << num_processes << std::endl;
-    std::cout << rank << std::endl;
+    int message;
+    if (rank == 0) {
+        message = 1337;
+        MPI_Send(&message, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    } else {
+        MPI_Recv(&message, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        std::cout << message << std::endl;
+    }
 
     MPI_Finalize();
     return 0;
