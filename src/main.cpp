0576e0a829eb1214f7b1a527034db01d97c0445b
Mateusz Warzyński <mathew.warzynski@gmail.com>
2019-05-24 20:10:00 +0200

    Moved generator code to src/

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-#include <mpi.h>
 #include <iostream>
+#include <mpi.h>
+#include "densematgen.h"
 
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
@@ -15,6 +16,16 @@ int main(int argc, char **argv) {
     } else {
         MPI_Recv(&message, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         std::cout << message << std::endl;
+    }
+
+    const int seed = 42;
+    const int size = 10;
+    for (int r = 0; r < size; r++) {
+        for (int c = 0; c < size; c++) {
+            const double entry = generate_double(seed, r, c);
+            std::cout << entry << " ";
+        }
+        std::cout << std::endl;
     }
 
     MPI_Finalize();
