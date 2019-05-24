10e9f4ac1cd936e42d14bda4fe396f367d3e4bd7
Mateusz Warzyński <mathew.warzynski@gmail.com>
2019-05-25 01:16:13 +0200

    WIP: Added algorithm steps description - main

--- src/main.cpp
+++ src/main.cpp
@@ -20,17 +20,21 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    if (rank == 0) {
-        MatrixCRS* matrix;
-        try {
-            matrix = parser::parse_sparse_matrix(arg->sparse_matrix_file);
-        } catch (std::exception &e) {
-            std::cout << "Parsing " << arg->sparse_matrix_file << ": " << e.what() << std::endl;
-            MPI_Finalize();
-            return 2;
-        }
-        std::cout << *matrix;
-    }
+    // Algorithm:
+    // 1. Our implementation must start from a data distribution for c = 1 (i.e., as if there is no replication).
+    //  Using a generator we supply, processes generate the dense matrix B in parallel (our generator is stateless,
+    //  so it might be used in parallel by multiple MPI processes; however, each element of the matrix must be
+    //  generated exactly once).
+    //
+    // 2. Process 0 loads the sparse matrix A from a CSR file (see bibliography for the description of the format) and
+    //  then sends it to other processes. Each process should receive only a part of the matrix that it will store for
+    //  data distribution for c = 1 (the coordinator should not send redundant data).
+    //
+    // Only after this initial data distribution, processes should contact their peers in replication groups and
+    // exchange their parts of matrices.
+    //
+    // Write two versions of your program. In a basic version, do not use any libraries for local (inside a process)
+    // matrix multiplication. In a second version, use MKL for local matrix multiplication.
 
     MPI_Finalize();
     return 0;
